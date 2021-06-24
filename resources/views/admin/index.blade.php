31f07686fd412ea16e53326dadbf3b16fa62ebe6
empleator <empleator5@gmail.com>
2021-06-25 00:52:14 +0200

    Segundo Comit

---
+++ resources/views/admin/index.blade.php
@@ -0,0 +1,18 @@
+@extends('adminlte::page')
+@section('title', 'WiEmpleo')
+
+@section('content_header')
+    <h1>WiEmpleo</h1>
+@stop
+
+@section('content')
+    <p>Welcome to this beautiful admin panel.</p>
+@stop
+
+@section('css')
+    <link rel="stylesheet" href="/css/admin_custom.css">
+@stop
+
+@section('js')
+    <script> console.log('Hi!'); </script>
+@stop
